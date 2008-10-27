a916a5d95ebc19c77cf0ec6e4dd223f84c66042f
leo <none@none>
2008-10-27 23:38:56 +0000

    --HG-- branch : xerial-core extra : convert_revision : svn%3Aae02f08e-27ec-0310-ae8c-8ba02fe2eafd/XerialJ/trunk/xerial-core%402604

--- xerial-core/src/main/java/org/xerial/util/shell/OptionParser.java
+++ xerial-core/src/main/java/org/xerial/util/shell/OptionParser.java
@@ -30,7 +30,6 @@ import java.util.HashSet;
 
 import org.xerial.core.StandardErrorCode;
 import org.xerial.util.cui.OptionParserException;
-import org.xerial.util.cui.OptionWithNoArgument;
 
 /**
  * A command-line option and argument parser
@@ -42,6 +41,85 @@ public class OptionParser
 {
     private boolean ignoreUnknownOption = false;
 
+    public OptionItem findOptionItem(OptionSchema schema, String optionName) throws OptionParserException
+    {
+        OptionItem optionItem = schema.getOption(optionName);
+        if (optionItem == null)
+        {
+            if (!ignoreUnknownOption)
+            {
+                throw new OptionParserException(StandardErrorCode.SYNTAX_ERROR, "unknown option: "
+                        + optionName);
+            }
+        }
+        return optionItem;
+    }
+    
+    enum ArgumentType { OPTION, ARGUMENT }  
+    
+    static class CommandLineToken
+    {
+        final ArgumentType optionOrArgument;
+        final String key;
+        final String value;
+        
+        public CommandLineToken(ArgumentType optionOrArgument, String key, String value)
+        {
+            this.optionOrArgument = optionOrArgument;
+            this.key = key;
+            this.value = value;
+        }
+
+    }
+    
+//    static class CommandLineScanner
+//    {
+//        private final String args[];
+//        private final OptionSchema schema;
+//        private int cursor = 0;
+//        
+//        public CommandLineScanner(String[] args, OptionSchema schema)
+//        {
+//            this.args = args;
+//            this.schema = schema;
+//        }
+//     
+//        public boolean hasNext()
+//        {
+//            return cursor < args.length;
+//        }
+//        
+//        public CommandLineToken nextToken()
+//        {
+//            String currentArg = args[cursor];
+//            if(currentArg.startsWith("--"))
+//            {
+//                String body = currentArg.substring(2);
+//                int splitPos = currentArg.indexOf('=');
+//                if (splitPos == -1)
+//                {
+//                    // no value is found
+//                    String longOptionName = currentArg.substring(2);
+//                    OptionItem optionItem = findOptionItem(schema, longOptionName); 
+//                }
+//                
+//            }
+//            
+//            cursor++;
+//            
+//            return null;
+//        }
+//         
+//        private OptionItem findOption(String name)
+//        {
+//        }
+//         
+//        
+//        
+//        
+//        
+//    }
+    
     
     public <OptionBean> OptionBean parse(String[] args, OptionBean bean) throws OptionParserException
     {
@@ -63,63 +141,65 @@ public class OptionParser
                 {
                     // no value is found
                     String longOptionName = currentArg.substring(2);
-                    OptionItem optionItem = schema.getOption(longOptionName);
-                    if (optionItem == null)
-                    {
-                        if (ignoreUnknownOption)
-                        {
-                            index++;
-                            continue;
-                        }
-                        else
-                        {
-                            throw new OptionParserException(StandardErrorCode.SYNTAX_ERROR, "unknown option --"
-                                    + longOptionName);
-                        }
-                    }
-
+                    OptionItem optionItem = findOptionItem(schema, longOptionName); 
+                    if(optionItem == null)
+                        continue;
+                    
                     if (optionItem.needsArgument())
                         throw new OptionParserException(StandardErrorCode.SYNTAX_ERROR,
                                 "parameter value is required for --" + longOptionName);
 
-                    optionItem.setOption(bean, args[++index]);
+                    optionItem.setOption(bean, "true");
                     activatedOption.add(optionItem.getOption());
                 }
                 else
                 {
                     // option is a (key, value) pair
-//                    String longOptionName = currentArg.substring(2, splitPos);
-//                    String value = currentArg.substring(splitPos + 1);
-//                    OptionSetter setter = schema.getOption(longOptionName);
-//                    if (setter == null)
-//                        if (ignoreUnknownOption)
-//                        {
-//                            index++;
-//                            continue;
-//                        }
-//                        else
-//                            throw new OptionParserException(StandardErrorCode.SYNTAX_ERROR, "unknown option --"
-//                                    + longOptionName);
-//
-//                    if(!setter.takesArgument())
-//                    {
-//                        throw new OptionParserException(StandardErrorCode.SYNTAX_ERROR, "syntax error --"
-//                                + longOptionName);
-//                    }
-//
-//                    setter.setOption(bean,args[++index]);
-//                    activatedOption.add(setter.getOption());
+                    String longOptionName = currentArg.substring(2, splitPos);
+                    String value = currentArg.substring(splitPos + 1);
+                    OptionItem optionItem = findOptionItem(schema, longOptionName);
+                    if (optionItem == null)
+                        continue;
+
+                    if(!optionItem.needsArgument())
+                    {
+                        throw new OptionParserException(StandardErrorCode.SYNTAX_ERROR, "syntax error --"
+                                + longOptionName);
+                    }
+
+                    optionItem.setOption(bean,args[++index]);
+                    activatedOption.add(optionItem.getOption());
                 }
 
             }
             else if (currentArg.startsWith("-"))
             {
                 // option with a leading hyphen
-                
+                String shortOptionList = currentArg.substring(1);
+                for(int i=0; i<shortOptionList.length(); i++)
+                {
+                    String shortOptionName = shortOptionList.substring(i, i+1);
+                    OptionItem optionItem = findOptionItem(schema, shortOptionName);
+                    if(optionItem == null)
+                        continue;
+                    
+                    if(optionItem.needsArgument()){
+                        if(shortOptionList.length() != 1)
+                            throw new OptionParserException(StandardErrorCode.SYNTAX_ERROR, String.format("short name option -%s with an arguments must be isolated", shortOptionName));
+                        
+                        optionItem.setOption(bean,args[++index]);
+                    }
+                    else
+                        optionItem.setOption(bean, "true");
+                    
+                    activatedOption.add(optionItem.getOption());
+                }
             }
             else
             {
                 // general argument
+                
+                
             }
 
         }
