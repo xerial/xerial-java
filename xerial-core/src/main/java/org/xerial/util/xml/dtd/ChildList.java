f7410b14d1c9f9833ada5db11e5fdf329d00bf27
leo <none@none>
2008-10-30 07:30:06 +0000

    dtd components
    --HG--
    branch : xerial-core
    extra : convert_revision : svn%3Aae02f08e-27ec-0310-ae8c-8ba02fe2eafd/XerialJ/trunk/xerial-core%402623

---
+++ xerial-core/src/main/java/org/xerial/util/xml/dtd/ChildList.java
@@ -0,0 +1,55 @@
+/*--------------------------------------------------------------------------
+ *  Copyright 2008 Taro L. Saito
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *--------------------------------------------------------------------------*/
+//--------------------------------------
+// XerialJ
+//
+// ElementList.java
+// Since: Oct 30, 2008 4:13:55 PM
+//
+// $URL$
+// $Author$
+//--------------------------------------
+package org.xerial.util.xml.dtd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChildList
+{
+    private ArrayList<ChildNode> childList = new ArrayList<ChildNode>();
+    private Occurence            occurence = Occurence.ONE;
+
+    public void addChild(ChildNode child)
+    {
+        childList.add(child);
+    }
+
+    public List<ChildNode> getChildNodeList()
+    {
+        return childList;
+    }
+
+    public Occurence getOccurence()
+    {
+        return occurence;
+    }
+
+    public void setOccurence(Occurence occurence)
+    {
+        this.occurence = occurence;
+    }
+
+}
