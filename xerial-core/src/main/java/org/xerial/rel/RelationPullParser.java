e73dad51a14db6be6dc0a163b310417e092d5b7d
leo <none@none>
2009-01-14 07:42:49 +0000

    --HG-- branch : xerial-core extra : convert_revision : svn%3Aae02f08e-27ec-0310-ae8c-8ba02fe2eafd/XerialJ/trunk/xerial-core%402841

--- xerial-core/src/main/java/org/xerial/rel/RelationPullParser.java
+++ xerial-core/src/main/java/org/xerial/rel/RelationPullParser.java
@@ -1,5 +1,5 @@
 /*--------------------------------------------------------------------------
- *  Copyright 2008 utgenome.org
+ *  Copyright 2009 Taro L. Saito
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  *  limitations under the License.
  *--------------------------------------------------------------------------*/
 //--------------------------------------
-// utgb-core Project
+// xerial-core Project
 //
 // ObjectStreamReader.java
 // Since: Jan 5, 2009
