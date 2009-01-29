0316b16bc46ad366b1762ce9f12a01948fa9f5e7
leo <none@none>
2009-01-29 13:35:06 +0000

    Added lexer state machine
    --HG--
    branch : xerial-core
    extra : convert_revision : svn%3Aae02f08e-27ec-0310-ae8c-8ba02fe2eafd/XerialJ/trunk/xerial-core%402910

---
+++ xerial-core/src/main/java/org/xerial/silk/impl/SilkLexerState.java
@@ -0,0 +1,85 @@
+/*--------------------------------------------------------------------------
+ *  Copyright 2009 Taro L. Saito
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *--------------------------------------------------------------------------*/
+//--------------------------------------
+// XerialJ
+//
+// SilkLexerState.java
+// Since: 2009/01/29 20:12:51
+//
+// $URL$
+// $Author$
+//--------------------------------------
+package org.xerial.silk.impl;
+
+import org.xerial.util.graph.Automaton;
+import org.xerial.util.graph.AutomatonCursor;
+
+/**
+ * Lexer state DFA
+ * 
+ * <pre>
+ *  DEFAULT -(NodeStart)-&gt; KEY 
+ *  KEY -&gt; (Colon) -&gt; OUT
+ *  OUT -&gt; (LParen) -&gt; IN
+ *  IN -&gt; (Colon) -&gt; IN
+ *   
+ *  ($current = ANY) -&gt; (Other) -&gt; ($current)
+ * </pre>
+ * 
+ * @author leo
+ * 
+ */
+public class SilkLexerState
+{
+    public static enum State {
+        INIT, KEY, OUT, IN
+    };
+
+    public static enum Symbol {
+        NodeStart, Colon, EnterParen
+    }
+
+    private final Automaton<State, Symbol> automaton = new Automaton<State, Symbol>();
+    private final AutomatonCursor<State, Symbol> cursor;
+
+    public SilkLexerState()
+    {
+        automaton.addTransition(State.INIT, Symbol.NodeStart, State.KEY);
+        automaton.addTransition(State.KEY, Symbol.Colon, State.OUT);
+        automaton.addTransition(State.OUT, Symbol.EnterParen, State.IN);
+
+        for (State each : State.values())
+            automaton.addStarTransition(each, each);
+
+        cursor = automaton.cursor(State.INIT);
+    }
+
+    public State transit(Symbol input)
+    {
+        return cursor.transit(input);
+    }
+
+    public State getCurrentState()
+    {
+        return cursor.getState();
+    }
+
+    public void reset()
+    {
+        cursor.reset(State.INIT);
+    }
+
+}
