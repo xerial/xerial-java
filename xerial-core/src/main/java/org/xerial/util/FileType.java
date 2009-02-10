f3f7f65733fef4c9e98f3089175cba19485a7514
leo <none@none>
2009-02-10 02:18:47 +0000

    --HG-- branch : xerial-core extra : convert_revision : svn%3Aae02f08e-27ec-0310-ae8c-8ba02fe2eafd/XerialJ/trunk/xerial-core%402953

---
+++ xerial-core/src/main/java/org/xerial/util/FileType.java
@@ -0,0 +1,112 @@
+/*--------------------------------------------------------------------------
+ *  Copyright 2009 Taro L. Saito
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *--------------------------------------------------------------------------*/
+//--------------------------------------
+// XerialJ
+//
+// FileType.java
+// Since: Feb 10, 2009 10:38:10 AM
+//
+// $URL$
+// $Author$
+//--------------------------------------
+package org.xerial.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * File type notation
+ * 
+ * @author leo
+ * 
+ */
+public enum FileType {
+
+    // image formats
+    JPEG("jpeg,jpg"),
+    PNG("png"),
+    GIF("gif"),
+    BMP("bmp"),
+    TIFF("tiff"),
+
+    // text formats
+    XML("xml"),
+    JSON("json"),
+    SILK("silk"),
+    TAB("tab"),
+    CSV("csv"),
+    FASTA("fasta"),
+    TEX("tex"),
+    TEXT("txt"),
+
+    // HTML formats
+    HTML("html,htm"),
+
+    // binary format
+    PDF("pdf"),
+    WORD("doc,docx"),
+    EXCEL("xls,xlsx"),
+    POWER_POINT("ppt,pptx"),
+
+    // compressed format
+    ZIP("zip"),
+    GZIP("gz"),
+    TAR("tar"),
+    TAR_GZ("tar.gz"),
+    BZIP2("bz2")
+
+    ;
+
+    private static HashMap<String, FileType> fileTypeTable = new HashMap<String, FileType>();
+    static
+    {
+        for (FileType eachType : FileType.values())
+        {
+            for (String eachExt : eachType.getFileExtList())
+                fileTypeTable.put(eachExt, eachType);
+        }
+    }
+
+    private ArrayList<String> fileExtList;
+
+    private FileType(String fileExtCSV)
+    {
+        String[] fileExt = fileExtCSV.split(",\\s*");
+        assert (fileExt != null);
+        fileExtList = new ArrayList<String>(fileExt.length);
+        for (String each : fileExt)
+        {
+            fileExtList.add(each);
+        }
+    }
+
+    public List<String> getFileExtList()
+    {
+        return fileExtList;
+    }
+
+    /**
+     * Retrieve file type
+     * 
+     * @param fileExt
+     * @return
+     */
+    public static FileType getFileType(String fileExt)
+    {
+        return fileTypeTable.get(fileExt.toLowerCase());
+    }
+}
