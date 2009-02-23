a6d05538d0af17aa5b2fb0939cd6d0327c0c19f0
leo <none@none>
2009-02-23 04:48:56 +0000

    --HG-- branch : xerial-core extra : convert_revision : svn%3Aae02f08e-27ec-0310-ae8c-8ba02fe2eafd/XerialJ/trunk/xerial-core%403004

---
+++ xerial-core/src/test/java/org/xerial/silk/Coordinate.java
@@ -0,0 +1,54 @@
+/*--------------------------------------------------------------------------
+ *  Copyright 2009 Taro L. Saito
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *--------------------------------------------------------------------------*/
+//--------------------------------------
+// XerialJ
+//
+// Coordinate.java
+// Since: 2009/02/18 20:09:09
+//
+// $URL$
+// $Author$
+//--------------------------------------
+package org.xerial.silk;
+
+public class Coordinate
+{
+    String group;
+    String species;
+    String revision;
+    String name;
+
+    public void setGroup(String group)
+    {
+        this.group = group;
+    }
+
+    public void setSpecies(String species)
+    {
+        this.species = species;
+    }
+
+    public void setRevision(String revision)
+    {
+        this.revision = revision;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
