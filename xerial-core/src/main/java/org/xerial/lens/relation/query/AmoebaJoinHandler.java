7312fbfe92617a01c76adb60f3c576c6b4ae2eec
leo <none@none>
2009-07-27 09:00:37 +0000

    --HG-- branch : xerial-core extra : convert_revision : svn%3Aae02f08e-27ec-0310-ae8c-8ba02fe2eafd/XerialJ/trunk/xerial-core%403495

--- xerial-core/src/main/java/org/xerial/lens/relation/query/AmoebaJoinHandler.java
+++ xerial-core/src/main/java/org/xerial/lens/relation/query/AmoebaJoinHandler.java
@@ -27,15 +27,43 @@ package org.xerial.lens.relation.query;
 import org.xerial.lens.relation.Node;
 import org.xerial.lens.relation.schema.Schema;
 
-public interface AmoebaJoinHandler
-{
+/**
+ * Interface of the amoeba join result handler. The tree data will be consumed
+ * in the depth-first manner, and newly found amoeba pair and tree nodes will be
+ * reported in the DFS order.
+ * 
+ * @author leo
+ * 
+ */
+public interface AmoebaJoinHandler {
+
+    /**
+     * This method will be invoked once before the amoeba join process starts
+     */
     public void init();
 
+    /**
+     * Invoked when a new amoeba node pair specified in the query set is found
+     * 
+     * @param schema
+     * @param coreNode
+     * @param attributeNode
+     * @throws Exception
+     */
     public void newAmoeba(Schema schema, Node coreNode, Node attributeNode) throws Exception;
 
+    /**
+     * Invoked when leaving a node
+     * 
+     * @param schema
+     * @param node
+     * @throws Exception
+     */
     public void leaveNode(Schema schema, Node node) throws Exception;
 
     /**
+     * Invoked when a text node belongs to a core node is found
+     * 
      * @param schema
      * @param coreNode
      * @param textNode
@@ -46,5 +74,8 @@ public interface AmoebaJoinHandler
      */
     public void text(Schema schema, Node coreNode, Node textNode, String text) throws Exception;
 
+    /**
+     * Invoked when reading the input stream has finished
+     */
     public void finish();
 }
